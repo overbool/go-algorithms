0a209f6567eecc2e2d25d5932dc47ae677cb9e2a
Overbool <overbool.xu@gmail.com>
2018-12-16 22:27:47 +0800

    sort/bubble: add done flag

--- sort/bubble.go
+++ sort/bubble.go
@@ -2,10 +2,13 @@ package sort
 
 func BubbleSort(a []int) {
   length := len(a)
-  for i := 0; i < length; i++ {
+  done := false
+  for i := 0; i < length && done; i++ {
+    done = false
     for j := 0; j < length - i - 1; j++ {
       if a[j] > a[j+1] {
         a[j], a[j+1] = a[j+1], a[j]
+        done = true
       }
     }
   }
